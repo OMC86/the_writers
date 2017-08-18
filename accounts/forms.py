a9860639adcbb44951fe41b57504884db46885db
Chris <christoal@outlook.com>
2017-08-18 11:16:38 +0800

    added stripe subscription form, view, url and templates

--- accounts/forms.py
+++ accounts/forms.py
@@ -33,3 +33,31 @@ class UserRegistrationForm(UserCreationForm):
 class UserLoginForm(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserSubscriptionForm(UserCreationForm):
+    MONTHS = [
+        'Jan', 'Feb', 'Mar', 'Apr', 'May', 'June',
+        'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'
+    ]
+    CHOOSE_MONTH = list(enumerate(MONTHS, 1))
+    CHOOSE_YEAR = [(i, i) for i in xrange(2017, 2038)]
+
+    credit_card_number = forms.CharField(label='Credit card numer')
+    cvv = forms.CharField(label='Security code (CVV)')
+    expiry_month = forms.ChoiceField(label='Exp Month', choices=CHOOSE_MONTH)
+    expiry_year = forms.ChoiceField(label='Exp Year', choices=CHOOSE_YEAR)
+    stripe_id = forms.CharField(widget=forms.HiddenInput)
+    password1 = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput
+    )
+    password2 = forms.CharField(
+        label='Confirm Password',
+        widget=forms.PasswordInput
+    )
+
+    class Meta:
+        model = User
+        fields = ['email', 'password1', 'password2', 'stripe_id']
+        exclude = ['username']
--- accounts/views.py
+++ accounts/views.py
@@ -2,8 +2,15 @@ from django.contrib import messages, auth
 from django.core.urlresolvers import reverse
 from django.shortcuts import render, redirect
 from django.template.context_processors import csrf
-from accounts.forms import UserRegistrationForm, UserLoginForm
+from accounts.forms import UserRegistrationForm, UserLoginForm, UserSubscriptionForm
+from accounts.models import User
 from django.contrib.auth.decorators import login_required
+from django.conf import settings
+import datetime
+import stripe
+import arrow
+
+stripe.api_key = settings.STRIPE_SECRET
 
 # This view renders the landing page
 def landing(request):
@@ -37,6 +44,56 @@ def register(request):
     args.update(csrf(request))
 
     return render(request, 'register.html', args)
+
+
+# This renders the registration form and registers users
+
+def subscribe(request):
+    if request.method == 'POST':
+        form = UserSubscriptionForm(request.POST)
+        if form.is_valid():
+            try:
+                customer = stripe.Customer.create(
+                    email=form.cleaned_data['email'],
+                    card=form.cleaned_data['stripe_id'],
+                    plan='WRITERS_MONTHLY',
+                )
+
+                if customer:
+                    user = form.save()
+                    user.stripe_id = customer.id
+                    user.subscription_end = arrow.now().replace(weeks=+4).datetime
+                    user.save()
+
+                    user = auth.authenticate(email=request.POST.get('email'),
+                                             password=request.POST.get('password1'))
+
+                    if user is not None:
+                        messages.success(request, "You have successfully subscribed")
+                        return redirect(reverse('home'))
+                    else:
+                        messages.error(request, "We were unable to take a payment with that card!")
+
+            except stripe.error.CardError, e:
+                messages.error(request, "Your card was declined!")
+    else:
+        today = datetime.date.today()
+        form = UserSubscriptionForm(initial={'expiry_month': today.month, 'expiry_year': today.year})
+
+    args = {'form': form, 'publishable': settings.STRIPE_PUBLISHABLE}
+    args.update(csrf(request))
+
+    return render(request, 'subscribe.html', args)
+
+
+def cancel_subscription(request):
+    try:
+        customer = stripe.Customer.retrieve(request.user.stripe_id)
+        customer.cancel_subscription(at_period_end=True)
+    except Exception, e:
+        messages.error(request, e)
+    return redirect('home')
+
 
 # Renders the login form and authenticates user
 def login(request):
