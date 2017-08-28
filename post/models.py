098e5398775daf99e7fe86b9efd9b54ca3abf6f8
Chris <christoal@outlook.com>
2017-08-28 15:25:38 +0800

    added competition model with methods to check time

--- post/models.py
+++ post/models.py
@@ -2,7 +2,7 @@
 from django.db import models
 from django.utils import timezone
 from writers import settings
-
+from datetime import datetime
 # Create your models here.
 
 
@@ -71,10 +71,38 @@ class Post(models.Model):
     vote_count = models.IntegerField(default=0)
     views = models.IntegerField(default=0)
     image = models.ImageField(upload_to="images", blank=True, null=True)
+    comp = models.ForeignKey('Competition', blank=True, null=True)
 
     def publish(self):
-        self.published_date = timezone.now()
+        self.date_published = timezone.now()
         self.save()
 
     def __unicode__(self):
         return self.title
+
+
+class Competition(models.Model):
+    title = models.CharField(max_length=50)
+    brief = models.CharField(max_length=300)
+    entry_period_start = models.DateTimeField(blank=True, null=True)
+    entry_period_fin = models.DateTimeField(blank=True, null=True)
+    vote_period_start = models.DateTimeField(blank=True, null=True)
+    vote_period_end = models.DateTimeField(blank=True, null=True)
+    is_active = models.BooleanField(default=False)
+    can_vote = models.BooleanField(default=False)
+
+    def _is_active(self):
+        now = timezone.now()
+        if self.entry_period_start < now < self.entry_period_fin:
+            self.is_active = True
+            self.save()
+
+    def _can_vote(self):
+        now = timezone.now()
+        if self.vote_period_start < now < self.vote_period_end:
+            self.can_vote = True
+            self.save()
+
+    def __unicode__(self):
+        return self.title
+
