5aa34c14261d19546ab2c18d38ec71717c4d5c72
Chris <christoal@outlook.com>
2017-08-29 17:02:31 +0800

    post urls update for delete and featured view

--- post/urls.py
+++ post/urls.py
@@ -7,8 +7,12 @@ urlpatterns = [
     url(r'^posts/(?P<id>\d+)/$', views.post_detail),
     url(r'^posts/new/$', views.new_post, name='new_post'),
     url(r'^posts/(?P<id>\d+)/edit$', views.edit_post, name='edit'),
+    url(r'^posts/(?P<id>\d+)/delete$', views.delete_post, name='delete'),
 
     # competition
     url(r'^competition/$', views.show_competition, name='view_comp'),
     url(r'^competition/entries/$', views.comp_entries, name='view_entries'),
+
+    # featured
+    url(r'^featured/$', views.featured, name='featured'),
 ]
