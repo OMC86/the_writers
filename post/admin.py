ae4401e207f32717d29f2baeb25220870936ddb9
Chris <christoal@outlook.com>
2017-09-06 13:04:57 +0800

    added vote model to admin

--- post/admin.py
+++ post/admin.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from .models import Post, Competition
+from .models import Post, Competition, Vote
 
 
 admin.site.register(Post)
 admin.site.register(Competition)
+admin.site.register(Vote)
