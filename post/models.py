04da2e6e13c2580103b33938b589698c97f1ea8f
Chris <christoal@outlook.com>
2017-09-06 11:40:49 +0800

    competition model methods to determin active competitions and added winner field, view updates to reflect changes in model

--- post/models.py
+++ post/models.py
@@ -2,30 +2,31 @@
 from django.db import models
 from django.utils import timezone
 from writers import settings
+from django.db.models import Count
 from datetime import datetime
 # Create your models here.
 
 
 class Post(models.Model):
 
-    POEM = 'PO'
-    SHORT = 'SH'
-    ESSAY = 'ES'
-    MEMOIR = 'ME'
-    LETTER = 'LE'
-    SCRIPT = 'SC'
-    SPEECH = 'SP'
-    EXPOSE = 'EX'
-    ARGUMENT = 'AR'
-    OPINION = 'OP'
-    COMEDY = 'CO'
-    DRAMA = 'DR'
-    HORROR = 'HO'
-    ROMANCE = 'RO'
-    TRAGEDY = 'TR'
-    TRAGICOM = 'TC'
-    FANTASY = 'FA'
-    MYTH = 'MY'
+    POEM = 'Poem'
+    SHORT = 'Short'
+    ESSAY = 'Essay'
+    MEMOIR = 'Memoir'
+    LETTER = 'Letter'
+    SCRIPT = 'Script'
+    SPEECH = 'Speech'
+    EXPOSE = 'Expose'
+    ARGUMENT = 'Argument'
+    OPINION = 'Opinion'
+    COMEDY = 'Comedy'
+    DRAMA = 'Drama'
+    HORROR = 'Horror'
+    ROMANCE = 'Romance'
+    TRAGEDY = 'Tragedy'
+    TRAGICOM = 'Tragic Comedy'
+    FANTASY = 'Fantasy'
+    MYTH = 'Myth'
 
     TYPE = (
         (None, ""),
@@ -60,18 +61,18 @@ class Post(models.Model):
     )
 
     author = models.ForeignKey(settings.AUTH_USER_MODEL)
-    category = models.CharField(max_length=2, choices=TYPE, default="")
-    genre = models.CharField(max_length=2, choices=GENRE, default="")
+    category = models.CharField(max_length=20, choices=TYPE, default="")
+    genre = models.CharField(max_length=20, choices=GENRE, default="")
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=3000)
     date_created = models.DateTimeField(auto_now_add=True)
     date_published = models.DateTimeField(blank=True, null=True)
     is_featured = models.BooleanField(default=False)
     is_entry = models.BooleanField(default=False)
-    vote_count = models.IntegerField(default=0)
     views = models.IntegerField(default=0)
     image = models.ImageField(upload_to="images", blank=True, null=True)
     comp = models.ForeignKey('Competition', blank=True, null=True)
+
 
     def publish(self):
         self.date_published = timezone.now()
@@ -81,6 +82,12 @@ class Post(models.Model):
         return self.title
 
 
+class Vote(models.Model):
+    voter = models.ForeignKey(settings.AUTH_USER_MODEL)
+    comp = models.ForeignKey('Competition')
+    post_id = models.ForeignKey('Post')
+
+
 class Competition(models.Model):
     title = models.CharField(max_length=50)
     brief = models.CharField(max_length=300)
@@ -88,20 +95,23 @@ class Competition(models.Model):
     entry_period_fin = models.DateTimeField(blank=True, null=True)
     vote_period_start = models.DateTimeField(blank=True, null=True)
     vote_period_end = models.DateTimeField(blank=True, null=True)
-    is_active = models.BooleanField(default=False)
-    can_vote = models.BooleanField(default=False)
+    winner = models.OneToOneField('Post', blank=True, null=True)
 
-    def _is_active(self):
+    def is_active(self):
         now = timezone.now()
-        if self.entry_period_start < now < self.entry_period_fin:
-            self.is_active = True
-            self.save()
+        return self.entry_period_start < now < self.vote_period_end
 
-    def _can_vote(self):
+    def can_enter(self):
         now = timezone.now()
-        if self.vote_period_start < now < self.vote_period_end:
-            self.can_vote = True
-            self.save()
+        return self.entry_period_start < now < self.entry_period_fin
+
+    def can_vote(self):
+        now = timezone.now()
+        return self.vote_period_start < now < self.vote_period_end
+
+    def get_winner(self):
+        now = timezone.now()
+        return self.vote_period_end < now
 
     def __unicode__(self):
         return self.title
--- post/views.py
+++ post/views.py
@@ -2,11 +2,14 @@
 from __future__ import unicode_literals
 
 from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
+from django.db.models import Count
 from django.utils import timezone
-from .models import Post, Competition
+from .models import Post, Competition, Vote
 from .forms import PostForm
-from .utils import activate
+
+
 import arrow
 
 
@@ -35,14 +38,17 @@ def new_post(request):
             if post.is_entry:
                 user = request.user
                 end = user.subscription_end
-                now = arrow.now()
+                now = timezone.now()
                 if now < end:
-                    activate()
-                    a = Competition.objects.get(is_active=True)
-                    post.comp = a
-                    post.save()
-
-                    return redirect(post_detail, post.pk)
+                    competition = Competition.objects.all()
+                    for comp in competition:
+                        if comp.can_enter():
+                            post.comp = comp
+                            post.save()
+                            return redirect(post_detail, post.pk)
+                    else:
+                        messages.error(request, "The entry period has either finished or not begun")
+                        return redirect(new_post)
                 else:
                     messages.error(request, "Please subscribe to enter competition")
                     return redirect(new_post)
@@ -66,14 +72,18 @@ def edit_post(request, id):
             if post.is_entry:
                 user = request.user
                 end = user.subscription_end
-                now = arrow.now()
+                now = timezone.now()
                 if now < end:
-                    activate()
-                    a = Competition.objects.get(is_active=True)
-                    post.comp = a
-                    post.save()
+                    competition = Competition.objects.all()
+                    for comp in competition:
+                        if comp.can_enter():
+                            post.comp = comp
+                            post.save()
+                            return redirect(post_detail, post.pk)
+                    else:
+                        messages.error(request, "The entry period has either finished or not begun")
+                        return redirect(new_post)
 
-                    return redirect(post_detail, post.pk)
                 else:
                     messages.error(request, "Please subscribe to enter competition")
                     return redirect(new_post)
@@ -94,19 +104,77 @@ def delete_post(request, id):
 
 
 def show_competition(request):
-    activate()
-    comp = Competition.objects.filter(is_active=True)
-    return render(request, 'competition/comp.html', {'comp': comp})
+    competition = Competition.objects.all()
+    for comp in competition:
+        if comp.is_active():
+            return render(request, 'competition/comp.html', {'comp': comp})
+    else:
+        return render(request, 'competition/comp.html', {'comp': comp})
 
 
 def comp_entries(request):
-    activate()
-    comp = Competition.objects.get(is_active=True)
     entries = Post.objects.filter(is_entry=True)
-    return render(request, 'competition/entrylist.html', {'entries': entries, 'comp': comp})
+    competition = Competition.objects.all()
+    for comp in competition:
+        if comp.is_active():
+            return render(request, 'competition/entrylist.html', {'entries': entries, 'comp': comp})
+    else:
+        return render(request, 'competition/entrylist.html', {'entries': entries, 'comp': comp})
+
+
+def winner(request):
+    competition = Competition.objects.all()
+    for comp in competition:
+        if comp.get_winner():
+            x = Vote.objects.filter(comp=comp)
+            winners = x.values_list('post_id').annotate(
+                vote_count=Count('post_id')).order_by('-vote_count')
+            winner = winners[0]     # the winner tuple post_id, votes
+            getentry = winner[0]    # get the post_id from the tuple
+            entry = Post.objects.get(id=getentry)
+            comp.winner = entry
+            comp.save()
+            return render(request, 'competition/winner.html', {'competition': competition, 'entry': entry})
+    else:
+        return render(request, 'competition/winner.html', {'competition': competition})
+
+
+def entry_detail(request, id):
+    post = get_object_or_404(Post, pk=id)
+    post.views += 1
+    post.save()
+    voteobjects = Vote.objects.filter(post_id=post).values('voter')
+    votes = voteobjects.all().count()
+    return render(request, "competition/entrydetail.html", {'post': post, 'votes': votes, 'voteobjects': voteobjects})
+
+
+@login_required
+def cast_vote(request, id):
+    post = get_object_or_404(Post, pk=id)
+    competition = Competition.objects.all()
+    for comp in competition:
+        if comp.can_vote():
+            new_vote, created = Vote.objects.get_or_create(voter=request.user,
+                                                           post_id=post, comp=comp)
+            if not created:
+                messages.info(request, "You have already voted for this entry")
+                return redirect(entry_detail, post.pk)
+            else:
+                messages.info(request, "Thanks for voting")
+                return redirect(entry_detail, post.pk)
+    else:
+        messages.info(request, "Voting closed")
+        return redirect(entry_detail, post.pk)
 
 
 def featured(request):
     posts = Post.objects.filter(is_featured=True, date_published__lte=timezone.now()
                                 ).order_by('-date_published')
     return render(request, 'featured/featuredlist.html', {'posts': posts})
+
+
+def featured_detail(request, id):
+    post = get_object_or_404(Post, pk=id)
+    post.views += 1
+    post.save()
+    return render(request, "featured/featureddetail.html", {'post': post})
