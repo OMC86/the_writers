341bda52484b40107e30dad1e8ca5fcc6ee229db
Chris <christoal@outlook.com>
2017-09-06 11:15:54 +0800

    added profile view to pages app with home template updates and css

--- pages/urls.py
+++ pages/urls.py
@@ -4,6 +4,7 @@ import views
 
 urlpatterns = [
 
+    url(r'^home/$', views.profile, name='home'),
     url(r'^pages/about/$', views.about, name='about'),
     url(r'^pages/contact/$', views.contact, name='contact'),
     url(r'^pages/faq/$', views.faq, name='faq'),
--- pages/views.py
+++ pages/views.py
@@ -2,9 +2,14 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from post.models import Post, Competition
+from accounts.models import User
+from django.utils import timezone
 
 # Create your views here.
 # This view renders the base template after login which is the profile home page
+
+
 def about(request):
     return render(request, 'about.html')
 
@@ -15,3 +20,18 @@ def contact(request):
 
 def faq(request):
     return render(request, 'faq.html')
+
+
+def profile(request):
+
+    users = User.objects.filter(subscription_end__gte=timezone.now())
+    prize = users.count()
+    post = Post.objects.filter(is_featured=True)
+    competition = Competition.objects.all()
+    for comp in competition:
+        if comp.is_active():
+            return render(request, 'home.html', {'post': post, 'comp': comp, 'prize': prize, 'users': users})
+    else:
+        return render(request, 'home.html', {'post': post, 'prize': prize, 'users': users})
+
+
