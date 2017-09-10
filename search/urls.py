ad075b7e70a4cac69ace54b5cedcac3d367a7994
Chris <christoal@outlook.com>
2017-09-10 15:32:42 +0800

    changed search so users can access selected post detail view from filtered query set

--- search/urls.py
+++ search/urls.py
@@ -1,16 +1,14 @@
 from django.conf.urls import url
-from django_filters.views import FilterView
-from filters import PostFilter
+
 import views
 
 urlpatterns = [
 
     # filter
-    url(r'^search/$', FilterView.as_view(filterset_class=PostFilter,
-                                         template_name='search.html'), name='search'),
+    url(r'^search/$', views.search, name='search'),
 
     # list all posts related to a single user
-    url(r'^search/(?P<id>\d+)/$', views.search_user, name='search_user'),
-    url(r'^search/(?P<id>\d+)/(?P<post_id>\d+)/$', views.user_post, name='user_post'),
+    url(r'^search/(?P<id>\d+)/$', views.search_detail, name='search_detail'),
+
 
 ]
--- search/views.py
+++ search/views.py
@@ -2,20 +2,22 @@
 from __future__ import unicode_literals
 
 from post.models import Post
-from accounts.models import User
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from filters import PostFilter
 # Create your views here.
 
 
-def search_user(request, id):
-    user = get_object_or_404(User, pk=id)
-    posts = Post.objects.filter(author=user)
-    return render(request, 'post_by_user.html', {'posts': posts, 'user': user})
+def search(request):
+    post_list = Post.objects.all()
+    post_filter = PostFilter(request.GET, queryset=post_list)
+    return render(request, 'search.html', {'posts': post_filter})
 
 
-def user_post(request, id, post_id):
-    user = get_object_or_404(User, pk=id)
-    post = get_object_or_404(Post, pk=post_id)
+
+def search_detail(request, id):
+    post = get_object_or_404(Post, pk=id)
     post.views += 1
     post.save()
-    return render(request, 'post_by_user_detail.html', {'post': post})
+    return render(request, 'search_detail.html', {'post': post})
+
