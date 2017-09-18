200d88ac0a75632a79d639e4a4224c82685e71aa
Chris <christoal@outlook.com>
2017-09-18 16:24:39 +0800

    changed winners to handle two or more winners

--- post/views.py
+++ post/views.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.contrib import messages, auth
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Count
 from django.utils import timezone
-from .models import Post, Competition, Vote
+from .models import Post, Competition
+from vote.models import Vote
 from accounts.models import User
 from .forms import PostForm
-
-
-import arrow
+from comments.forms import CommentForm
 
 
 # renders a list of posts descending order
@@ -32,10 +31,11 @@ def post_list(request):
 
 
 def post_detail(request, id):
+    x = timezone.now()
     post = get_object_or_404(Post, pk=id)
     post.views += 1
     post.save()
-    return render(request, "posts/postdetail.html", {'post': post})
+    return render(request, "posts/postdetail.html", {'post': post, 'x': x})
 
 
 @login_required 
@@ -64,7 +64,7 @@ def new_post(request):
                             post.save()
                             return redirect(post_detail, post.pk)
                     else:
-                        messages.error(request, "The entry period has either finished or not begun")
+                        messages.error(request, "The entry period has ended")
                         return redirect(new_post)
                 else:
                     messages.error(request, "Please subscribe to enter competition")
@@ -132,39 +132,57 @@ def delete_post(request, id):
 
 
 def show_competition(request):
+    x = timezone.now()
     competition = Competition.objects.all()
+    subscribers = User.objects.filter(subscription_end__gte=timezone.now())
+    prize = subscribers.count()
     for comp in competition:
         """
         get the currently active competition
         """
         if comp.is_active():
-            return render(request, 'competition/comp.html', {'comp': comp})
+            return render(request, 'competition/comp.html', {'comp': comp, 'x': x, 'prize': prize})
     else:
         return render(request, 'competition/comp.html')
 
 
 def comp_entries(request):
+    x = timezone.now()
     entries = Post.objects.filter(is_entry=True)
     competition = Competition.objects.all()
     for comp in competition:
         if comp.is_active():
-            return render(request, 'competition/entrylist.html', {'entries': entries, 'comp': comp})
+            return render(request, 'competition/entrylist.html', {'entries': entries, 'comp': comp, 'x': x})
     else:
         return render(request, 'competition/entrylist.html')
 
 
 def entry_detail(request, id):
+    x = timezone.now()
     post = get_object_or_404(Post, pk=id)
     post.views += 1
     post.save()
     voteobjects = Vote.objects.filter(post_id=post)
     votes = voteobjects.all().count()
-    for vote in voteobjects:
-        if vote.voter == request.user:
-            return render(request, "competition/entrydetail.html",
-                          {'post': post, 'votes': votes, 'vote': vote})
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            return redirect(entry_detail, post.id)
     else:
-        return render(request, "competition/entrydetail.html", {'post': post, 'votes': votes})
+        form = CommentForm()
+        for vote in voteobjects:
+            if vote.voter == request.user:
+                return render(request, "competition/entrydetail.html",
+                              {'post': post, 'votes': votes, 'vote': vote, 'form': form, 'x': x})
+        else:
+            form = CommentForm()
+            return render(request, "competition/entrydetail.html",
+                          {'post': post, 'votes': votes, 'form': form, 'x': x})
 
 
 @login_required
@@ -199,31 +217,58 @@ def winners(request):
         comps = paginator.page(paginator.num_pages)
 
     for comp in competitions:
+        posts = Post.objects.filter(is_winner=True)
         if comp.is_active():
-            return render(request, 'competition/winnerlist.html', {'comps': comps})
-        elif comp.winner is None:
+            return render(request, 'competition/winnerlist.html', {'comps': comps, 'posts': posts})
+        elif not comp.winner:
             try:
-                comp = Competition.objects.get(winner=None)
+                # Get the competition with no winner and all vote objects associated with it
+                comp = Competition.objects.get(winner=False)
                 x = Vote.objects.filter(comp=comp)
+
+                # Count how many times the same post_id occurs in post objects for x comp
                 winners = x.values_list('post_id').annotate(
-                vote_count=Count('post_id')).order_by('-vote_count')
-                winner = winners[0]     # the winner tuple post_id, votes
-                getentry = winner[0]    # get the post_id from the tuple
-                entry = Post.objects.get(id=getentry)
-                comp.winner = entry
+                    vote_count=Count('post_id'))
 
-                subscribers = User.objects.filter(subscription_end__gte=timezone.now())
-                prize = subscribers.count()
-                comp.prize = prize
-                comp.save()
-                return render(request, 'competition/winnerlist.html', {'comps': comps})
+                # find max votes. Add all post_id with max votes to tied list (index[0] = post_id, index[1] = votes)
+                max_val = max(x[1] for x in winners)
+                tied = []
+                for v in winners:
+                    if v[1] == max_val:
+                        tied.append(v[0])
+                else:
+                    # if there is a tie length of tied will be more than one
+                    if len(tied) > 1:
+                        champs = Post.objects.filter(id__in=tied)
+                        for c in champs:
+                            c.is_winner = 1
+                            c.save()
+                        comp.winner = 1
+                        subscribers = User.objects.filter(subscription_end__gte=timezone.now())
+                        prize = subscribers.count()
+                        comp.prize = (prize % len(tied))
+                        comp.save()
+                        return render(request, 'competition/winnerlist.html', {'comps': comps, 'posts': posts})
 
+                    else:
+                        getentry = tied[0]
+                        entry = Post.objects.get(id=getentry)
+                        entry.is_winner = 1
+                        entry.save()
+
+                        comp.winner = 1
+                        subscribers = User.objects.filter(subscription_end__gte=timezone.now())
+                        prize = subscribers.count()
+                        comp.prize = prize
+                        comp.save()
+                        return render(request, 'competition/winnerlist.html', {'comps': comps, 'posts': posts})
+            # Catches 500 server error when there's an active comp with no winner assigned
             except Exception:
-                return render(request, 'competition/winnerlist.html', {'comps': comps})
+                return render(request, 'competition/winnerlist.html', {'comps': comps, 'posts': posts})
 
         # if all competitions have winners and there's no active comps
         else:
-            return render(request, 'competition/winnerlist.html', {'comps': comps})
+            return render(request, 'competition/winnerlist.html', {'comps': comps, 'posts': posts})
 
 
 
@@ -234,7 +279,17 @@ def winner_detail(request, id):
     post.save()
     voteobjects = Vote.objects.filter(post_id=post)
     votes = voteobjects.all().count()
-    return render(request, 'competition/winnerdetail.html', {'post': post, 'votes': votes})
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            return redirect(winner_detail, post.id)
+    else:
+        form = CommentForm()
+        return render(request, 'competition/winnerdetail.html', {'post': post, 'votes': votes, 'form': form})
 
 
 
@@ -255,7 +310,19 @@ def featured(request):
 
 
 def featured_detail(request, id):
+    x = timezone.now()
     post = get_object_or_404(Post, pk=id)
     post.views += 1
     post.save()
-    return render(request, "featured/featureddetail.html", {'post': post})
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            return redirect(featured_detail, post.id)
+    else:
+        form = CommentForm()
+        return render(request, "featured/featureddetail.html", {'post': post, 'form': form, 'x': x})
