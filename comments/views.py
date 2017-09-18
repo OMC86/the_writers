0b81efd512f6f673592547329044eea440803038
Chris <christoal@outlook.com>
2017-09-18 16:07:42 +0800

    added template and styles to comments app

--- comments/views.py
+++ comments/views.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect, get_object_or_404
-from .forms import CommentForm
-from post.models import Post
 # Create your views here.
 
 
