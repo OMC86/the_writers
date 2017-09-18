1d78fee1294c046718b97da4867227480ba9fd19
Chris <christoal@outlook.com>
2017-09-18 16:37:15 +0800

    changed filter so user can type username also removed is_entry

--- search/filters.py
+++ search/filters.py
@@ -1,10 +1,10 @@
 from post.models import Post
-import django_filters
+import django_filters as filters
 
 
-class PostFilter(django_filters.FilterSet):
-
-    title = django_filters.CharFilter(name='title', lookup_expr='icontains')
+class PostFilter(filters.FilterSet):
+    author__username = filters.CharFilter(label="Author's username")
+    title = filters.CharFilter(name='title', lookup_expr='icontains')
     class Meta:
         model = Post
-        fields = ['author', 'category', 'genre', 'is_entry', 'comp']
+        fields = ['author__username', 'title', 'category', 'genre', 'comp']
