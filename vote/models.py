464eaea057b5011c9d587b3fbd77004b0da6c046
Chris <christoal@outlook.com>
2017-09-27 17:30:49 +0800

    added related name to vote model

--- vote/models.py
+++ vote/models.py
@@ -8,6 +8,6 @@ from writers import settings
 
 # Create your models here.
 class Vote(models.Model):
-    voter = models.ForeignKey(settings.AUTH_USER_MODEL)
-    comp = models.ForeignKey(Competition)
-    post_id = models.ForeignKey(Post)
+    voter = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='votes')
+    comp = models.ForeignKey(Competition, related_name='votes')
+    post_id = models.ForeignKey(Post, related_name='votes')
