ce9263f798f81c90e126b3fedcd35d7ebdd8c8d4
Chris <christoal@outlook.com>
2017-09-27 17:47:08 +0800

    update urls

--- pages/urls.py
+++ pages/urls.py
@@ -6,7 +6,5 @@ urlpatterns = [
 
     url(r'^home/$', views.profile, name='home'),
     url(r'^pages/about/$', views.about, name='about'),
-    url(r'^pages/contact/$', views.contact, name='contact'),
-    url(r'^pages/faq/$', views.faq, name='faq'),
 
     ]
