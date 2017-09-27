005683958d4528cce8b368f7d2a1578de668d943
Chris <christoal@outlook.com>
2017-09-27 17:35:11 +0800

    added template tags

---
+++ post/templatetags/post_extras.py
@@ -0,0 +1,35 @@
+from django import template
+from django.utils import timezone
+from post.models import Post, Competition
+import random
+import arrow
+
+register = template.Library()
+
+# https://stackoverflow.com/questions/28837511/django-template-how-to-randomize-order-when-populating-page-with-objects
+
+
+@register.filter
+def shuffle(arg):
+    tmp = list(arg)[:]
+    random.shuffle(tmp)
+    return tmp
+
+
+@register.simple_tag
+def get_total_posts():
+    return Post.objects.all().count()
+
+
+@register.simple_tag
+def get_total_comps():
+    return Competition.objects.all().count()
+
+
+@register.simple_tag
+def total_prize():
+    comps = Competition.objects.filter(winner=True)
+    prize_list = []
+    for c in comps:
+        prize_list.append(c.prize)
+    return sum(prize_list)
