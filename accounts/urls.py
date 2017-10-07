6c225efab9d589cf5d80b51fd97d7f7d7842ec76
Chris <christoal@outlook.com>
2017-10-07 12:10:08 +0800

    user can now retrieve forgotten password via email urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 import views
+
 
 urlpatterns = [
     url(r'^$', views.landing),
@@ -9,6 +11,13 @@ urlpatterns = [
     url(r'^accounts/subscribe/$', views.subscribe, name='subscribe'),
     url(r'^accounts/cancel/$', views.cancel_subscription, name='cancel'),
     url(r'^accounts/upload/$', views.upload, name='upload'),
+
+    # Password reset
+    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 
 
     # Webhooks
