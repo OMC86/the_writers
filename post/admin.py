28fa2ff433a2281e3394b6ff4734c7aca217b703
Chris <christoal@outlook.com>
2017-10-09 15:18:49 +0800

    custom validations to prevent admin from setting invalid dates or more that one competition at a time

--- post/admin.py
+++ post/admin.py
@@ -1,8 +1,40 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 from .models import Post, Competition
+from django import forms
+
+# Set validation errors for non consecutive dates and multiple competitions
+class NewCompetitionForm(forms.ModelForm):
+    class Meta:
+        model = Competition
+        exclude = (
+            'winner', 'prize'
+        )
+
+    def clean(self):
+        comp_list = Competition.objects.order_by('-vote_period_end')
+        comp = comp_list[0]
+
+        entry_start = self.cleaned_data.get('entry_period_start')
+        entry_fin = self.cleaned_data.get('entry_period_fin')
+        vote_start = self.cleaned_data.get('vote_period_start')
+        vote_end = self.cleaned_data.get('vote_period_end')
+
+        if entry_fin < entry_start or vote_end < vote_start or vote_start < entry_fin:
+            raise forms.ValidationError('Invalid dates')
+        elif comp.is_active():
+            raise forms.ValidationError('Only one active competition is permitted at a time')
+        return self.cleaned_data
+
+
+class CompetitionAdmin(admin.ModelAdmin):
+    form = NewCompetitionForm
+    list_display = (
+        'title', 'brief', 'entry_period_start', 'vote_period_end', 'winner', 'prize',
+    )
 
 
 admin.site.register(Post)
-admin.site.register(Competition)
+admin.site.register(Competition, CompetitionAdmin)
+
 
