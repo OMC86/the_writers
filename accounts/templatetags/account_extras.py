6f0d1cc50f15e1d97e6445f3aa1c06889c05f70e
Chris <christoal@outlook.com>
2017-10-11 13:15:46 +0800

    removed redundant template tag

--- accounts/templatetags/account_extras.py
+++ accounts/templatetags/account_extras.py
@@ -5,19 +5,6 @@ from accounts.models import User
 
 register = template.Library()
 
-@register.filter
-def check_subscription(user):
-    subscribers = []
-    user = User.objects.filter(subscription_end__gte=timezone.now())
-    for u in user:
-        subscribers.append(u)
-    return subscribers
-
-
-@register.simple_tag
-def count_subscribers():
-    return User.objects.filter(subscription_end__gte=timezone.now()).count()
-
 
 @register.simple_tag
 def count_users():
