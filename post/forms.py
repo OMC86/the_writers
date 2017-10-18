fe95ca6572992ddddcda1680600ab2ed5a5ed100
Chris <christoal@outlook.com>
2017-10-18 17:34:36 +0800

    help text update

--- post/forms.py
+++ post/forms.py
@@ -11,22 +11,10 @@ class PostForm(ModelForm):
         fields = ('category', 'genre', 'tags', 'title', 'content', 'image', 'is_featured', 'is_entry',)
         labels = {
             'is_featured': _('Make this post public'),
-            'is_entry': _('Enter this post in current competition')
+            'is_entry': _('Enter this post in current competition'),
+            'image-clear': _('')
         }
         help_texts = {
-            'is_entry': _('You can only enter a competition if one is currently active.')
-        }
-
-
-class PostEditForm(ModelForm):
-
-    class Meta:
-        model = Post
-        fields = ('category', 'genre', 'tags', 'title', 'content', 'image', 'is_featured', 'is_entry',)
-        labels = {
-            'is_featured': _('Make this post public'),
-            'is_entry': _('Enter this post in current competition')
-        }
-        help_texts = {
-            'is_entry': _('You can only enter a competition if one is currently active.')
+            'is_entry': _('Important! Copy your post before entering a competition; If you are not a subscriber or'
+                          ' the entry period is over when you save, you will loose the post.')
         }
